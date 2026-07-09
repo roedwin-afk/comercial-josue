c891b0b72c1e30db5e3197f4ef3f0f7f87116910
Edwin Rivera <gaedwin92@gmail.com>
2026-07-09 11:10:38 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// Ajusta a tu repo real cuando lo subas a GitHub.
+export default defineConfig({
+  site: "https://roedwin-afk.github.io",
+  base: "/comercial-josue/",
+});
